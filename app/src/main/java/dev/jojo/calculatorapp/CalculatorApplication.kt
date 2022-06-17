9b6f9c31fe79d5af7eb7e5d259a5293f8b198960
Munde-J <mundeangelah@gmail.com>
2022-06-17 06:33:50 +0300

    textView

--- app/src/main/java/dev/jojo/calculatorapp/CalculatorApplication.kt
+++ app/src/main/java/dev/jojo/calculatorapp/CalculatorApplication.kt
@@ -14,10 +14,6 @@ class CalculatorApplication : AppCompatActivity() {
     lateinit var btnModulus: Button
     lateinit var etNumber1: EditText
     lateinit var etNumber2: EditText
-
-
-    lateinit var tilNumber1: TextInputLayout
-    lateinit var tilNumber2: TextInputLayout
     lateinit var tvResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,34 +24,31 @@ class CalculatorApplication : AppCompatActivity() {
         btnSubtraction = findViewById(R.id.btnSubtraction)
         btnDivision = findViewById(R.id.btnDivision)
         btnModulus = findViewById(R.id.btnModulus)
-
-        tilNumber1 = findViewById(R.id.tilNumber1)
-        tilNumber2 = findViewById(R.id.tilNumber2)
         etNumber1 = findViewById(R.id.etNumber1)
         etNumber2 = findViewById(R.id.etNumber2)
 
         tvResult = findViewById(R.id.tvResult)
 
         btnAddition.setOnClickListener {
-            val number1 = tilNumber1.toString().toDouble()
-            val number2 = tilNumber2.toString().toDouble()
+            val number1 = etNumber1.toString().toDouble()
+            val number2 = etNumber2.toString().toDouble()
             addition(number1, number2)
 
 
         }
         btnSubtraction.setOnClickListener {
-            val number1 = tilNumber1.toString().toDouble()
-            val number2 = tilNumber2.toString().toDouble()
+            val number1 = etNumber1.text.toString().toDouble()
+            val number2 = etNumber2.text.toString().toDouble()
             subtraction(number1, number2)
         }
         btnDivision.setOnClickListener {
-            val number1 = tilNumber1.toString().toDouble()
-            val number2 = tilNumber2.toString().toDouble()
+            val number1 = etNumber1.text.toString().toDouble()
+            val number2 = etNumber2.text.toString().toDouble()
             division(number1, number2)
         }
         btnModulus.setOnClickListener {
-            val number1 = tilNumber1.toString().toDouble()
-            val number2 = tilNumber2.toString().toDouble()
+            val number1 = etNumber1.text.toString().toDouble()
+            val number2 = etNumber2.text.toString().toDouble()
             modulus(number1, number2)
 
         }
@@ -67,19 +60,19 @@ class CalculatorApplication : AppCompatActivity() {
         tvResult.text = add.toString()
     }
 
-    fun subtraction(Number:Double, Number2:Double) {
-        var subtract = Number - Number2
-        tvResult.text = subtract.toString()
+    fun subtraction(Number1:Double, Number2:Double) {
+        var add = Number1 - Number2
+        tvResult.text = add.toString()
     }
 
-    fun division(Number:Double, Number2:Double) {
-        var divide = Number / Number2
-        tvResult.text = divide.toString()
+    fun division(Number1:Double, Number2:Double) {
+        var add = Number1 / Number2
+        tvResult.text = add.toString()
     }
 
-    fun modulus(Number:Double, Number2:Double) {
-        var modulus = Number % Number2
-        tvResult.text = modulus.toString()
+    fun modulus(Number1:Double, Number2:Double) {
+        var add = Number1 % Number2
+        tvResult.text = add.toString()
     }
 }
 
